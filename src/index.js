e0fe9511a407d65d8fb211692a8cfe9721b08b8b
Matt Orndorff <ornmatt321@gmail.com>
2022-03-10 13:22:03 -0500

    isOneTestNormal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
 import './less/index.less'
 
 // Your code goes here!
-console.log('hello world')
+console.log('hello world');
+
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`);
+    const heading = document.querySelector('h1');
+    heading.textContent = "READY TO GO!";
+    
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+        .then(text => {
+            heading.textContent += text
+        })
+    })
+    document.body.addEventListener('click', evt => {
+        evt.target.classList.toggle('mirror')
+    })
+
+    document.body.addEventListener('dblclick', evt => {
+        evt.target.outerHTML = '';
+    })
+
+    window.addEventListener('keydown', evt => {
+        if(evt.key == 6) {
+            document.body.outerHTML = 'YOU RAN ORDER 66';
+        }
+    })
+    document.body.addEventListener('mousemove', evt => {
+        const { clientX, clientY} = evt
+        // console.log(`mouse is at ${clientX}, ${clientY}`)
+    })
+
+    const destinations = document.querySelectorAll('.destination')
+    // console.log(destinations)
+    for (let destination of destinations) {
+        destination.addEventListener('mouseenter', evt => {
+// console.log(destinations)
+destination.style.fontWeight = 'bold'
+        })
+        destination.addEventListener('mouseleave', () => {
+            destination.style.fontWeight = 'initial'
+        }, 500)
+    }
+}
